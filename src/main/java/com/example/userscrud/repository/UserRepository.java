0a17df443db2cb028f0699f8caab199f0fb62333
unknown <joesi8302@gmail.com>
2024-09-27 10:47:50 -0700

    asdas

--- src/main/java/com/example/userscrud/repository/UserRepository.java
+++ src/main/java/com/example/userscrud/repository/UserRepository.java
@@ -1,24 +1,32 @@
 package com.example.userscrud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.userscrud.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	@Query(value = "SELECT * FROM user WHERE email = ?1", nativeQuery = true)
-	  User findByEmailAddress(String emailAddress);
 
-	@Query(value = "SELECT * FROM user WHERE name = ?name", nativeQuery = true)
+	@Query(value = "SELECT * FROM user WHERE email = ?1", nativeQuery = true)
+	User findByEmailAddress(String emailAddress);
+
+
+	@Query(value = "SELECT * FROM user WHERE name = ?1", nativeQuery = true)
 	User findByName(String name);
 
-	@Query(value = "DELETE FROM user WHERE email = :email", nativeQuery = true)
-	User deleteByEmail(String email);
 
-	@Query(value = "SELECT COUNT(*) FROM user WHERE name = :name", nativeQuery = true)
+	@Modifying
+	@Query(value = "DELETE FROM user WHERE email = ?1", nativeQuery = true)
+	void deleteByEmail(String email);
+
+
+	@Query(value = "SELECT COUNT(*) FROM user WHERE name = ?1", nativeQuery = true)
 	Integer getUserNameCount(String name);
 
-	@Query(value = "DELETE FROM user WHERE name = :name", nativeQuery = true)
-	User deleteByName(String name);
+	
+	@Modifying
+	@Query(value = "DELETE FROM user WHERE name = ?1", nativeQuery = true)
+	void deleteByName(String name);
 }
